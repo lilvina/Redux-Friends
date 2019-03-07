944e9193908b15a964eef550fab719590acc465c
lilvina <davinaleo2007@yahoo.com>
2019-03-07 15:34:34 -0800

    login page appears

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,25 +1,52 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    username: '',
+    password: '',
+    isLoggedIn: false,
+    waiting: false
+  }
+
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  login = event => {
+    event.preventDefault()
+
+    this.setState({
+      waiting: true
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Friends Redux</h1>
+        {this.state.waiting ? (<div className="wait">waiting...</div>) : (
+          <form onSubmit={this.login}>
+            <input type="text"
+              name="username"
+              onChange={this.handleChange}
+              placeholder="username"
+              value={this.state.username}
+              required />
+
+              <input type="text"
+                name="password"
+                onChange={this.handleChange}
+                placeholder="password"
+                value={this.state.password}
+                required />
+
+              <input type="submit" />
+          </form>
+        )}
       </div>
     );
   }
